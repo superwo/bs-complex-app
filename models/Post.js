4a942edb142cfa5f46c44866a09932a97e348e73
Radu <avadanii@tiscali.it>
2019-09-03 10:43:33 +0300

    Post CRUD

--- models/Post.js
+++ models/Post.js
@@ -3,11 +3,13 @@ const postsCollection = require('../db')
   .collection('posts');
 const ObjectID = require('mongodb').ObjectID;
 const User = require('./User');
+const sanitizeHTML = require('sanitize-html');
 
-let Post = function(data, userid) {
+let Post = function(data, userid, requestedPostId) {
   this.data = data;
   this.errors = [];
   this.userid = userid;
+  this.requestedPostId = requestedPostId;
 };
 
 Post.prototype.cleanUp = function() {
@@ -20,8 +22,14 @@ Post.prototype.cleanUp = function() {
 
   // get rid of any bogus properties
   this.data = {
-    title: this.data.title.trim(),
-    body: this.data.body.trim(),
+    title: sanitizeHTML(this.data.title.trim(), {
+      allowedTags: [],
+      allowedAttributes: []
+    }),
+    body: sanitizeHTML(this.data.body.trim(), {
+      allowedTags: [],
+      allowedAttributes: []
+    }),
     createdDate: new Date(),
     author: ObjectID(this.userid)
   };
@@ -42,8 +50,8 @@ Post.prototype.create = function() {
       // save post into database
       postsCollection
         .insertOne(this.data)
-        .then(() => {
-          resolve();
+        .then(info => {
+          resolve(info.ops[0]._id);
         })
         .catch(() => {
           this.errors.push('Please try again later.');
@@ -51,6 +59,39 @@ Post.prototype.create = function() {
         });
     } else {
       reject(this.errors);
+    }
+  });
+};
+
+Post.prototype.update = function() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let post = await Post.findSingleById(this.requestedPostId, this.userid);
+      if (post.isVisitorOwner) {
+        // actually update the db
+        let status = await this.actuallyUpdate();
+        resolve(status);
+      } else {
+        reject();
+      }
+    } catch (err) {
+      reject();
+    }
+  });
+};
+
+Post.prototype.actuallyUpdate = function() {
+  return new Promise(async (resolve, reject) => {
+    this.cleanUp();
+    this.validate();
+    if (!this.errors.length) {
+      await postsCollection.findOneAndUpdate(
+        { _id: new ObjectID(this.requestedPostId) },
+        { $set: { title: this.data.title, body: this.data.body } }
+      );
+      resolve('success');
+    } else {
+      resolve('failure');
     }
   });
 };
@@ -117,6 +158,22 @@ Post.findByAuthorId = function(authorId) {
     { $match: { author: authorId } },
     { $sort: { createdDate: -1 } }
   ]);
+};
+
+Post.delete = function(postIdToDelete, currentUserId) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let post = await Post.findSingleById(postIdToDelete, currentUserId);
+      if (post.isVisitorOwner) {
+        await postsCollection.deleteOne({ _id: new ObjectID(postIdToDelete) });
+        resolve();
+      } else {
+        reject();
+      }
+    } catch (err) {
+      reject();
+    }
+  });
 };
 
 module.exports = Post;
